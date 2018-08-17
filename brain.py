f19c80a06325701e5080a24971a24375565e295c
bastosvinicius <bastosvinicius@live.com>
2018-08-17 03:59:11 -0300

    Update brain.py

--- brain.py
+++ brain.py
@@ -1,35 +1,40 @@
 #!/bin/python3.4
 
-print("hi, i'm brain")
-print("now i'll help you to do some things that zabbix can offer you with python lang")
-
-# py modules
-
-import os, sys
-import pkg_resources
+import os, sys, socket, pkg_resources
 from pkg_resources import DistributionNotFound, VersionConflict
 
-# check pre-reqs
+# local paths
 
-dependencies = {
-  'termcolor': '1.1.0',
-  'progressbar': '2.5',
-  'pyzabbix': '0.7.4'
-}
+localpath = os.getcwd()
+localmodules = localpath+"/modules"
+localconfig = localpath+"/.config"
+sys.path.insert(0, localmodules)
+
+# local modules
+
+import menu, internet
+
+print("hi, i'm brain")
+print("i'll help you to do some things that zabbix can offer you with python lang")
+
+try:
+  internet.internet()
+except:
+  print("no internet connection")
 
 try:
   pkg_resources.require(dependencies)
   print("all dependencies are satisfied")
 except:
-  print("failed to load dependencies")
-  os.system('pip3.4 install -r .config/requirements.txt > /dev/null')
+  print("not all dependencies were met")
+  print("trying to install the dependencies")
+  try:
+    os.system('pip3.4 install -r '+localconfig+'/modules-requiriments > /dev/null')
+  except:
+    print("failed to install dependencies")
+    print("quitting")
+    quit()
 
-# local modules
-
-localpath = os.getcwd()
-localmodules = localpath+"/modules"
-sys.path.insert(0, localmodules)
-
-import menu
+# py modules
 
 menu.menu()
