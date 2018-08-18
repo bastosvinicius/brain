b4ac7e911f434f7de12e80638bcf5d5b51e1ac03
bastosvinicius <bastosvinicius@live.com>
2018-08-17 23:37:48 -0300

    Update mzabbix.py

--- modules/mzabbix.py
+++ modules/mzabbix.py
@@ -1,12 +1,36 @@
+#!/bin/python3.4
 from termcolor import colored
-import menu
+from pyzabbix import ZabbixAPI
+import menu, mzabbix_extract, getpass, csv, os, urllib3, traceback, logging
 
 def mzabbix():
+  def login():
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    zapi = ZabbixAPI("https://<ZABBIX URL>/api_jsonrpc.php")
+    zapi.session.auth = (user, password)
+    zapi.session.verify = False
+    zapi.timeout = 10
+    zapi.login(user, password)
+    print("connected to Zabbix API Versionv %s" % zapi.api_version())
+
+  def loginerror():
+    os.system('clear')
+    print(colored('impossible connect to Zabbix API', 'red', attrs=['bold']))
+    print(colored('verify the API URL or ur credentials', 'red', attrs=['bold']))
+    menu.menu()
+
   print("")
-  print(colored('hello', 'blue'), ("d(-_-)b"), colored('world', 'blue'))
+  print(colored('d(-_-)b', 'blue'))
   print("welcome to brain - zabbix\n")
   print("this script helps u to extract infos from zabbix, mass insert of hosts, items, triggers or users, get information from hosts and install, uninstall or upgrade agents")
   print("")
+  user = input("input ur user: ")
+  password = getpass.getpass("enter ur password: ")
+  try:
+    login()
+  except Exception as e:
+    logging.error(traceback.format_exc())
+    loginerror()
   print("make ur choice\n")
   print("[1] - extract data")
   print("[2] - insert data")
@@ -20,7 +44,7 @@ def mzabbix():
   option = input("make ur choice: ")
   print("")
   if option == '1':
-    print("extract data menu\n")
+    mzabbix_extract.zabbix_extract()
   elif option == '2':
     print("insert data menu\n")
   elif option == '3':
